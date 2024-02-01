b1d49bb9c3890d9f450a98dba88aabc2756b1f6f
albertoISC <josealberto100897@gmail.com>
2024-02-01 10:04:03 -0600

    Modificación de la vista welcome con el uso de diferentes etiquetas de HTML

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,52 @@
-<h1>COMPONENTES DEL SISTEMA</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>COMPONENTES DEL SISTEMA</h1>
+    <hr>
+
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing <br>
+        elit. Doloribus asperiores dolorem non, eveniet vero odio  <br> 
+        voluptates temporibus accusamus, <a href="#">possimus sed</a> possimus sed tempora pariatur, <br>
+         nam ea repellendus perferendis! Deserunt ratione eligendi ad.</p>
+
+        
+
+        <form action="">
+            <label for="Name">Nombre</label>
+            <input type="Nombre">
+            <br>
+            <label for="Last name">Apellido</label>
+            <input type="Apellido">
+            <br>
+            <label for="Email">Correo</label>
+            <input type="Correo ">
+            <br>
+
+         <label for="boton">Boton</label>
+         <button>Enviar</button>
+        </form>
+         <hr>
+        <h3>Lista</h3>
+         <ul>
+             <li>Elemento 1</li>
+             <li>Elemento 2</li>
+             <li>Elemento 3</li>
+             <li>Elemento 4</li>
+
+
+         </ul>
+
+         in
+
+
+
+    
+</body>
+</html>
+
