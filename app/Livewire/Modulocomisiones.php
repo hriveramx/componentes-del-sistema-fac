ada6cd81bc735c113b69dbe6d84afb6ee566d7aa
albertoISC <josealberto100897@gmail.com>
2024-02-13 17:28:36 -0600

    Practica con livewire

--- app/Livewire/Modulocomisiones.php
+++ app/Livewire/Modulocomisiones.php
@@ -30,12 +30,121 @@ class Modulocomisiones extends Component
     public $brokers_1_porcentaje;
     public $brokers_2_porcentaje;
     public $brokers_3_porcentaje;
+
+    //**************** */
+
+    public $monto_negativo;
+    public $fecha;
+    public $cliente;
+    public $ticket;
+
+    public $IdBroker;
+    public $IdTicket;
+    public $ClienteB;
+    public $Comision;
+    public $Monto;
+    public $FechaB;
+
+    public $IdBroker2;
+    public $IdTicket2;
+    public $ClienteB2;
+    public $Comision2;
+    public $Monto2;
+    public $FechaB2;
+    
+    public $IdBroker3;
+    public $IdTicket3;
+    public $ClienteB3;
+    public $Comision3;
+    public $Monto3;
+    public $FechaB3;
+
+    public $brokers_1_selectCombo;
+    public $brokers_2_selectCombo;
+    public $brokers_3_selectCombo;
+
+
+    //********* */
+
+    
+    public $IdGrupo;
+    public $IdTicketTablaG;
+    public $ClienteTablaG;
+    public $Concepto;
+    public $BancoReceptop;
+    public $CuentaReceptop;
+    public $ClaveR;
+    public $Comision_Gmex;
+    public $FechaBG;
+    public $Total_G;
+
+    public $IdGrupo2;
+    public $IdTicketTablaG2;
+    public $ClienteTablaG2;
+    public $Concepto2;
+    public $BancoReceptop2;
+    public $CuentaReceptop2;
+    public $ClaveR2;
+    public $Comision_Gmex2;
+    public $FechaBG2;
+    public $Total_G2;
+
+
+    public $IdGrupo3;
+    public $IdTicketTablaG3;
+    public $ClienteTablaG3;
+    public $Concepto3;
+    public $BancoReceptop3;
+    public $CuentaReceptop3;
+    public $ClaveR3;
+    public $Comision_Gmex3;
+    public $FechaBG3;
+    public $Total_G3;
+
+    public $IdGrupo4;
+    public $IdTicketTablaG4;
+    public $ClienteTablaG4;
+    public $Concepto4;
+    public $BancoReceptop4;
+    public $CuentaReceptop4;
+    public $ClaveR4;
+    public $Comision_Gmex4;
+    public $FechaBG4;
+    public $Total_G4;
+
+    public $IdGrupo5;
+    public $IdTicketTablaG5;
+    public $ClienteTablaG5;
+    public $Concepto5;
+    public $BancoReceptop5;
+    public $CuentaReceptop5;
+    public $ClaveR5;
+    public $Comision_Gmex5;
+    public $FechaBG5;
+    public $Total_G5;
+
+    public $IdGrupo6;
+    public $IdTicketTablaG6;
+    public $ClienteTablaG6;
+    public $Concepto6;
+    public $BancoReceptop6;
+    public $CuentaReceptop6;
+    public $ClaveR6;
+    public $Comision_Gmex6;
+    public $FechaBG6;
+    public $Total_G6;
+
+
     
     public function mount()
     {
 
+        
         $this->comisionGeneral = "0";
         $this->basecomision = "total";
+        $this->brokers_1_selectCombo = "0";
+        $this->brokers_2_selectCombo = "0";
+        $this->brokers_3_selectCombo = "0";
         $this->baseComisionPorcentaje = "5.00%";
         
         $this->CantidadTotal = "3480000";
@@ -50,9 +159,38 @@ class Modulocomisiones extends Component
             ];
         }
 
+        $this->fecha = "2024-02-14";
+        $this->cliente = "HB";
+        $this->ticket = "FR1";
+
+        $this->IdGrupo = "Balance";
+        $this->IdGrupo3 = "Balance";
+        $this->IdGrupo4 = "Balance";
+        $this->IdGrupo5 = "Balance";
+        $this->IdGrupo6 = "Balance";
+        $this->BancoReceptop = "Saldos Clientes";
+        $this->BancoReceptop4 = "Saldos Brokers";
+        $this->BancoReceptop5 = "Saldos Brokers";
+        $this->BancoReceptop6 = "Saldos Brokers";
+        $this->ClaveR4 = "Broker";
+        $this->ClaveR5 = "Broker";
+        $this->ClaveR6 = "Broker";
+
+
+        $this->ClaveR = "Cliente";
+        $this->IdGrupo2 = "QMEX";
+        $this->BancoReceptop2 =  "BBVA BANCOMER";
+        $this->CuentaReceptop2 ="012180001208795095";
+        $this->ClaveR2 = "85901865320331400";
+
+        $this->BancoReceptop3 = "INGRESOS";
+        $this->CuentaReceptop3 = "Comisiones";
+        $this->ClaveR3 = "NA";
+
        // $this->calcularPorcentajeComisionGmex();
 
         $this->CalcularCantidadComision();
+        $this->llenadoTablaBrokers();
     }
 
 
@@ -74,6 +212,7 @@ class Modulocomisiones extends Component
             $this->cantidadBroker_1 = (floatval($this->brokers_1_porcentaje) * $total)/100;
             $this->cantidadBroker_2 = (floatval($this->brokers_2_porcentaje) * $total)/100;
             $this->cantidadBroker_3 = (floatval($this->brokers_3_porcentaje) * $total)/100;
+            $this->monto_negativo = (floatval($this->pocentajeComisionGmex) * -$total)/100;
             
         }else
         {
@@ -82,12 +221,169 @@ class Modulocomisiones extends Component
             $this->cantidadBroker_1 = (floatval($this->brokers_1_porcentaje) * $subtotal)/100;
             $this->cantidadBroker_2 = (floatval($this->brokers_2_porcentaje) * $subtotal)/100;
             $this->cantidadBroker_3 = (floatval($this->brokers_3_porcentaje) * $subtotal)/100;
+            $this->monto_negativo = (($total/1.16) * -floatval($this->pocentajeComisionGmex))/100;
+            //$this->monto_negativo = (floatval($total)/1.16) * -$this->pocentajeComisionGmex/100;
 
         }
         
 
+        $this->llenadoTablaBrokers();
+        $this->llenadoTablaG();
+
     }
 
+
+        //TABLA BROKERS
+        public function llenadoTablaBrokers(){
+
+
+            if($this->brokers_1_selectCombo == "0"){
+
+                $this->IdBroker = "";
+            $this->IdTicket = "";
+            $this->ClienteB = "";
+            $this->Comision = "";
+            $this->Monto = "";
+            $this->FechaB = "";
+
+            }else{
+                $this->IdBroker = $this->brokers_1_selectCombo;
+                $this->IdTicket = $this->ticket;
+                $this->ClienteB = $this->cliente;
+                $this->Comision = $this->brokers_1_porcentaje ."%";
+                $this->Monto = $this->cantidadBroker_1;
+                $this->FechaB = $this->fecha;
+
+
+            }
+
+            if($this->brokers_2_selectCombo == "0"){
+
+                $this->IdBroker2 = "";
+            $this->IdTicket2 = "";
+            $this->ClienteB2 = "";
+            $this->Comision2 = "";
+            $this->Monto2 = "";
+            $this->FechaB2 = "";
+
+            }else{
+                $this->IdBroker2 = $this->brokers_2_selectCombo;
+                $this->IdTicket2 = $this->ticket;
+                $this->ClienteB2 = $this->cliente;
+                $this->Comision2 = $this->brokers_2_porcentaje . '%';
+                $this->Monto2 = $this->cantidadBroker_2;
+                $this->FechaB2 = $this->fecha;
+
+
+            }
+
+            if($this->brokers_3_selectCombo == "0"){
+
+                $this->IdBroker3 = "";
+            $this->IdTicket3 = "";
+            $this->ClienteB3 = "";
+            $this->Comision3 = "";
+            $this->Monto3 = "";
+            $this->FechaB3 = "";
+
+            }else{
+                $this->IdBroker3 = $this->brokers_3_selectCombo;
+                $this->IdTicket3 = $this->ticket;
+                $this->ClienteB3 = $this->cliente;
+                $this->Comision3 = $this->brokers_3_porcentaje . '%';
+                $this->Monto3 = $this->cantidadBroker_3;
+                $this->FechaB3 = $this->fecha;
+
+
+            }
+
+            $this->llenadoTablaG();
+
+
+        }
+
+        public function llenadoTablaG(){
+
+
+            $this->IdTicketTablaG = $this->ticket;
+            $this->ClienteTablaG = $this->cliente;
+            $this->CuentaReceptop = 'CLIENTE: ' . $this->cliente;
+            $this->FechaBG = $this->fecha;
+            
+
+            $this->IdTicketTablaG2 = $this->ticket;
+            $this->ClienteTablaG2 = $this->cliente;
+            $this->Concepto2 = $this->IdTicketTablaG2 . $this->IdGrupo2 ;
+            $this->Concepto = $this->Concepto2;
+            $this->Total_G2 = $this->CantidadTotal;
+            $this->Comision_Gmex2 =$this->cantidadComisionGmex;
+            $this->FechaBG2 = $this->fecha;
+
+            $this->IdTicketTablaG3 = $this->IdTicketTablaG2;
+            $this->ClienteTablaG3 = $this->ClienteTablaG2;
+            $this->Concepto3 = $this->Concepto2;
+            $this->Total_G3 = -$this->cantidadComisionGmex;
+            $this->FechaBG3 = $this->fecha;
+
+            $this->IdTicketTablaG4 = $this->IdTicketTablaG2;
+            $this->ClienteTablaG4 = $this->ClienteTablaG2;
+            $this->Concepto4 = $this->Concepto2;
+            $this->FechaBG4 = $this->fecha;
+
+            $this->IdTicketTablaG5 = $this->IdTicketTablaG4;
+            $this->ClienteTablaG5 = $this->ClienteTablaG4;
+            $this->Concepto5 = $this->Concepto4;
+            $this->FechaBG5 = $this->fecha;
+
+            $this->IdTicketTablaG6 = $this->IdTicketTablaG5;
+            $this->ClienteTablaG6 = $this->ClienteTablaG5;
+            $this->Concepto6 = $this->Concepto5;
+            $this->FechaBG6 = $this->fecha;
+
+
+
+            $this->CuentaReceptop4 = 'Broker: ' . $this->brokers_1_selectCombo;
+            $this->CuentaReceptop5 = 'Broker: ' . $this->brokers_2_selectCombo;
+            $this->CuentaReceptop6 = 'Broker: ' . $this->brokers_3_selectCombo;
+
+            $this->Total_G4 = null;
+            $this->Total_G5 = null;
+            $this->Total_G6 = null;
+            
+            if($this->IdBroker != null){
+
+                $this->Total_G4 = -$this->Monto;
+            }
+
+            if($this->IdBroker2 != null){
+
+                $this->Total_G5 = -$this->Monto2;
+            }
+
+            if($this->IdBroker3 != null){
+
+                $this->Total_G6 = -$this->Monto3;
+            }
+            
+
+
+            $this->Total_G = (-$this->Total_G2)-($this->Total_G3 + $this->Total_G4 + $this->Total_G5 + $this->Total_G6);
+            //$this->Total_G4 = -$this->Monto;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
 
     public function calcularPorcentajeComisionGmex()
     {
--- resources/views/livewire/modulocomisiones.blade.php
+++ resources/views/livewire/modulocomisiones.blade.php
@@ -25,7 +25,8 @@
 
             
                 <td rowspan="2" class="text-center" style="vertical-align: middle">
-                    <select name="selectComisionBroker_1" id="selectComisionBroker_1" class="form-control" wire:model="brokers_1_selectCombo">
+                    <select name="selectComisionBroker_1" id="selectComisionBroker_1" class="form-control" wire:model="brokers_1_selectCombo" wire:change="llenadoTablaBrokers">
+                        <option value="0"></option>
                         <option value="HB">HB</option>
                         <option value="MV">MV</option>
                         <option value="MT">MT</option>
@@ -50,7 +51,8 @@
                 </td>   
 
                 <td rowspan="2" class="text-center" style="vertical-align: middle">
-                    <select name="selectComisionBroker_1" id="selectComisionBroker_1" class="form-control" wire:model="brokers_1_selectCombo">
+                    <select name="selectComisionBroker_2" id="selectComisionBroker_2" class="form-control" wire:model="brokers_2_selectCombo" wire:change="llenadoTablaBrokers">
+                        <option value="0"></option>
                         <option value="HB">HB</option>
                         <option value="MV">MV</option>
                         <option value="MT">MT</option>
@@ -76,7 +78,8 @@
 
 
                 <td rowspan="2" class="text-center" style="vertical-align: middle">
-                    <select name="selectComisionBroker_1" id="selectComisionBroker_1" class="form-control" wire:model="brokers_1_selectCombo">
+                    <select name="selectComisionBroker_3" id="selectComisionBroker_3" class="form-control" wire:model="brokers_3_selectCombo" wire:change="llenadoTablaBrokers">
+                        <option value="0"></option>
                         <option value="HB">HB</option>
                         <option value="MV">MV</option>
                         <option value="MT">MT</option>
@@ -119,6 +122,163 @@
     <input type="number" wire:model="CantidadTotal">
     <label for="">Subtotal</label>
     <input type="number" wire:model="CantidadSubtotal">
+    <label for="">Cliente</label>
+    <input type="text" wire:model="cliente">
+    <label for="">Ticket</label>
+    <input type="text" wire:model="ticket">
+    <label for="">Fecha</label>
+    <input type="Date" wire:model="fecha">
+
+
+
+
+
+    <table>
+
+        <tr>
+
+            <td>Monto Negativo</td>
+                <td><input class="form-control" wire:model="monto_negativo" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+ 
+                
+        </tr>
+
+
+    </table>
+<hr>
+
+<table class="table table-light">
+    <td>Id Broker</td>
+    <td>Id Ticket</td>
+    <td>Cliente</td>
+    <td>Comision</td>
+    <td>Monto</td>
+    <td>Fecha</td>
+    <tr>
+            <td><input class="form-control" wire:model="IdBroker" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+            <td><input class="form-control" wire:model="IdTicket" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+            <td><input class="form-control" wire:model="ClienteB" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+            <td><input class="form-control" wire:model="Comision" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+            <td><input class="form-control" wire:model="Monto" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+            <td><input class="form-control" wire:model="FechaB" type="Date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+    <tr>
+        <td><input class="form-control" wire:model="IdBroker2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicket2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteB2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Monto2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaB2" type="Date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+
+    <tr>
+        <td><input class="form-control" wire:model="IdBroker3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicket3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteB3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Monto3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaB3" type="Date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+
+</table>
+
+<hr>
+
+<table class="table table-light">
+    <td>ID Grupo</td>
+    <td>Id Ticket</td>
+    <td>Cliente / CC / Ticket</td>
+    <td>Concepto</td>
+    <td>Banco Receptor</td>
+    <td>Cta Receptora</td>
+    <td>Clave de Rastro</td>
+    <td>Total</td>
+    <td>comision Gmex</td>
+    <td>Fecha</td>
+
+    <tr>
+        <td><input class="form-control" wire:model="IdGrupo" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicketTablaG" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteTablaG" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Concepto" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="BancoReceptop" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="CuentaReceptop" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClaveR" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Total_G" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision_Gmex" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaBG" type="date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+    
+    <tr>
+        <td><input class="form-control" wire:model="IdGrupo2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicketTablaG2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteTablaG2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Concepto2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="BancoReceptop2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="CuentaReceptop2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClaveR2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Total_G2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision_Gmex2" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaBG2" type="date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+   
+    <tr>
+        <td><input class="form-control" wire:model="IdGrupo3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicketTablaG3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteTablaG3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Concepto3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="BancoReceptop3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="CuentaReceptop3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClaveR3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Total_G3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision_Gmex3" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaBG3" type="date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+
+    <tr>
+        <td><input class="form-control" wire:model="IdGrupo4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicketTablaG4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteTablaG4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Concepto4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="BancoReceptop4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="CuentaReceptop4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClaveR4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Total_G4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision_Gmex4" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaBG4" type="date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+
+    <tr>
+        <td><input class="form-control" wire:model="IdGrupo5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicketTablaG5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteTablaG5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Concepto5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="BancoReceptop5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="CuentaReceptop5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClaveR5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Total_G5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision_Gmex5" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaBG5" type="date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+
+    <tr>
+        <td><input class="form-control" wire:model="IdGrupo6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="IdTicketTablaG6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClienteTablaG6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Concepto6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="BancoReceptop6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="CuentaReceptop6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="ClaveR6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Total_G6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="Comision_Gmex6" type="text" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+        <td><input class="form-control" wire:model="FechaBG6" type="date" placeholder="">@error('cantidadBroker_2') <p class="text-xs text-red">{{ $message }}</p> @enderror</td>
+    </tr>
+
+
+</table>
+
+
+
 
 
 </div>
